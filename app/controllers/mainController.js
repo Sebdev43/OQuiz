f65919ab75288aae3623cea8c19f6b9019828f99
Sebdev43 <sebastien.robert@oclock.school>
2024-04-10 10:17:35 +0200

    build: :construction: Visualiser les titres, la description et les auteurs des quiz sur la page d’accueil

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,6 +1,24 @@
+const { Quiz, User, Tag } = require('../models/index');
+
 const mainController = {
-  index(req, res) {
-    res.render('home');
+  async index(req, res) {
+    try {
+      const quizzes = await Quiz.findAll({
+        include: [{
+          model: User,
+          as: 'author',
+        },
+        {
+          model: Tag,
+          as: 'tags',
+        },
+        ],
+      });
+      res.render('home', { quizzes });
+    } catch (error) {
+      console.error('Error fecthing quizzes', error);
+      res.status(500).send('Error fecthing quizzes');
+    }
   },
 };
 
